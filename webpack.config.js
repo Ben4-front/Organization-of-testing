d8baceca9680ea49a95d11d22cb10f9b8d5bfab4
Ben4 <benymany511@gmail.com>
2025-12-09 19:15:27 +0300

    Fix webpack config and add loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,50 @@
-// ---------- конфигурационный файл webpack: ----------
-const { merge } = require('webpack-merge'); // извлекаем функцию merge()
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
-const commonConfig = require('./webpack.common.config');
-const productionConfig = require('./webpack.production.config');
-const developmentConfig = require('./webpack.development.config');
-
-module.exports = (env) => {
-  if (env.development) {
-    return merge(commonConfig, developmentConfig); // слияние 2ух конфигурационных файлов
-  }
-  return merge(commonConfig, productionConfig); // слияние 2ух конфигурационных файлов
+module.exports = {
+  // Убираем Warning, задавая режим явно
+  mode: 'production', 
+  
+  entry: './src/js/app.js',
+  
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    // Очищать папку dist перед сборкой
+    clean: true,
+    // Настройка для картинок (куда их класть в dist)
+    assetModuleFilename: 'images/[name][ext]',
+  },
+  
+  module: {
+    rules: [
+      // 1. Правило для JavaScript (Babel)
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      // 2. Правило для CSS
+      // Позволяет импортировать 'file.css' в JS
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      // 3. Правило для Картинок (PNG, SVG, JPG, GIF)
+      // Webpack 5 имеет встроенный Asset Modules, loader больше не нужен
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+  ],
 };
